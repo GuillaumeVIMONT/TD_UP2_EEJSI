b4c86962c9ec0a4789b199255cc86741ee31d65f
GuillaumeVIMONT <vimontguillaume@gmail.com>
2018-03-06 02:09:21 +0100

    Update config.py

--- TD_2/config.py
+++ TD_2/config.py
@@ -1,10 +1,10 @@
 class TwitterAuth:
 	# Go to http://dev.twitter.com and create an app. 
 	# The consumer key and secret will be generated for you after
-	consumer_key="Pw1GazuppiQeLRIejBSsA"
-	consumer_secret="y6FTjap7lgGgYepZzib3HfrEcoHfK9Sa7MmObOZiCU"
+	consumer_key=""
+	consumer_secret=""
 
 	# After the step above, you will be redirected to your app's page.
 	# Create an access token under the the "Your access token" section
-	access_token="2207034565-hHb8Y8BndQ0eTIQDDhvHmkLYJ4o6DPLf54WEkP3"
-	access_token_secret="nCm7K7zGszg18Fc89AUYsCDOM9cy67TNEpkTAzxRd9rfz"
+	access_token=""
+	access_token_secret=""
