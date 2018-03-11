9dc71850b8a59878532a132d11c16ebca4b0158b
Guillaume VIMONT <vimontguillaume@gmail.com>
2018-03-11 19:25:06 +0100

    Update generate_graph.py

--- TD_2/generate_graph.py
+++ TD_2/generate_graph.py
@@ -1,47 +1,30 @@
 import json
 import time
-#edge = []
-def create_graph(data):
-    current_milli_time = lambda: int(round(time.time() * 1000))
-    edge = []
-    all_data = json.loads(data)
-    user_screen_name = all_data['user']['screen_name']
-    timestamp_ms = all_data['timestamp_ms']
-    try:
-        hashtags1 = "#"+all_data['entities']['hashtags'][0]['text']
-        edge_1 = (user_screen_name, hashtags1, timestamp_ms)
-        edge.append((edge_1))
-        if 'hashtags1' in locals():
-            hashtags2 = "#"+all_data['entities']['hashtags'][1]['text']
-            edge_2 = (user_screen_name, hashtags2, timestamp_ms)
-            edge.append((edge_2))
-            if 'hashtags2' in locals():
-                hashtags3 = "#"+all_data['entities']['hashtags'][2]['text']
-                edge_3 = (user_screen_name, hashtags3, timestamp_ms)
-                edge.append((edge_3))
-                if 'hashtags3' in locals():
-                    hashtags4 = "#"+all_data['entities']['hashtags'][3]['text']
-                    edge_4 = (user_screen_name, hashtags4, timestamp_ms)
-                    edge.append((edge_4))
-    except:
-        pass
-    try:
-        user_mentions_screen_name1 = all_data['entities']['user_mentions'][0]['screen_name']
-        edge_5 = (user_screen_name, user_mentions_screen_name1, timestamp_ms)
-        edge.append((edge_5))
-        if 'user_mentions_screen_name1' in locals():
-            user_mentions_screen_name2 = all_data['entities']['user_mentions'][1]['screen_name']
-            edge_6 = (user_screen_name, user_mentions_screen_name2, timestamp_ms)
-            edge.append((edge_6))
-            if 'user_mentions_screen_name2' in locals():
-                user_mentions_screen_name3 = all_data['entities']['user_mentions'][2]['screen_name']
-                edge_7 = (user_screen_name, user_mentions_screen_name3, timestamp_ms)
-                edge.append((edge_7))
-                if 'user_mentions_screen_name3' in locals():
-                    user_mentions_screen_name4 = all_data['entities']['user_mentions'][3]['screen_name']
-                    edge_8 = (user_screen_name, user_mentions_screen_name4, timestamp_ms)
-                    edge.append((edge_8))
-    except:
-        pass
 
-    return edge
+def create_graph(data):
+	current_milli_time = lambda: int(round(time.time() * 1000))
+	edge_list = []
+	hashtags_list = []
+	user_mentions_list = []
+	all_data = json.loads(data)
+	user_screen_name = all_data['user']['screen_name']
+	timestamp_ms = all_data['timestamp_ms']
+	if len(all_data['entities']['hashtags']) > 0:
+		a=0
+		for i in all_data['entities']['hashtags']:
+			hashtags_list.append(all_data['entities']['hashtags'][a]['text'])
+			a+=1
+		a=0
+	if len(all_data['entities']['user_mentions']) > 0:
+		b=0
+		for i in all_data['entities']['user_mentions']:
+			user_mentions_list.append(all_data['entities']['user_mentions'][b]['screen_name'])
+			b+=1
+		b=0
+	if len(hashtags_list) > 0:
+		for i in hashtags_list:
+			edge_list.append((user_screen_name, "#"+i, timestamp_ms))
+	if len(user_mentions_list) > 0:
+		for i in user_mentions_list:
+			edge_list.append((user_screen_name, i, timestamp_ms))
+	return edge_list
