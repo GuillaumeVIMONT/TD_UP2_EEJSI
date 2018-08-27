4d7bffc380c938e8accf50f4a59f30e7caab9b56
Guillaume VIMONT <vimontguillaume@gmail.com>
2018-08-27 14:50:51 +0200

    Create 1.py

---
+++ TD_3/1.py
@@ -0,0 +1,7 @@
+import requests
+import json
+
+r = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+bitcoin_data = dict(r.json())
+bitcoin_value = bitcoin_data["bpi"]["USD"]["rate_float"]
+print("Actual Bitcoin pricing ", bitcoin_value, "$")
