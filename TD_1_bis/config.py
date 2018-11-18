155593e1bb44e7a663f0fec42ec2d4cff4f3bc79
Guillaume VIMONT <vimontguillaume@gmail.com>
2018-11-18 20:55:04 +0100

    Create config.py

---
+++ TD_1_bis/config.py
@@ -0,0 +1,7 @@
+class TwitterAuth:
+	# Go to http://dev.twitter.com and create an app. 
+	# The consumer key and secret will be generated for you after
+	consumer_key=""
+	consumer_secret=""
+	access_token=""
+	access_token_secret=""
