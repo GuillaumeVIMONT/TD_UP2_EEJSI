d45d22da0f759cebe095fe67b14529516746d11c
Guillaume VIMONT <vimontguillaume@gmail.com>
2018-11-21 18:27:47 +0100

    Update main.py

--- TD_1_bis/main.py
+++ TD_1_bis/main.py
@@ -20,26 +20,122 @@ now = datetime.datetime.now()
 
 print("Before start Twitter capture we need some informations")
 # Reservoir Sampling Size
-window_k = input("1/3 Please enter number of edges in the reservoir (eg 200): ")
+window_k = input("1/4 Please enter number of edges in the reservoir (eg 200): ")
 window_k = int(window_k)
 
 # tracking Twitter Stream
-tracking = input("2/3 Please enter tracking word (eg CNN) if multi keywords (eg CNN Foxnews): ")
+tracking = input("2/4 Please enter tracking word (eg CNN) if multi keywords (eg CNN Foxnews): ")
 
-timeout = input("3/3 Please enter duration of the capture in minutes (eg 30): ")
+threshold = input("3/4 Please enter threshold for the connected component export (eg 5) : ")
+timeout = input("4/4 Please enter duration of the capture in minutes (eg 30): ")
 timeout = int(timeout)*60000
 start = int(round(time.time() * 1000))
 
 print("Capture in progress")
 
 
-def write_edge_reservoir(edge_reservoir):
+def components(edges):
+  global d
+  d={}
+  n=0
+  for e in edges:
+     a=e[0]
+     b=e[1]
+     if a in d.keys(): d[a].add(b)
+     else: d[a]=set([b])
+     if b in d.keys(): d[b].add(a)
+     else: d[b]=set([a])
+     
+     
+  #print ("Dict=", d, len(d))
+  n=len(d)
+  m=0
+  #Breadth-first search from first point, first component
+  a=list(d.keys())[0]
+
+  #dc keeps b:i  if i is the length of the shortest path from a to b in the first component
+  dc={}
+  dc[a]=0
+  for b in d[a]: 
+    if b in dc.keys(): pass
+    else: dc[b]=1
+
+  #Initialize S ans S1, Start iterating
+  S=d[a]
+  comp=[]
+  S1=set([a])
+  S=S.union(S1)
+  #print("S=",S)
+  while S > S1:
+      S1=S
+      for u in S:
+         S=S.union(d[u])
+         for v in d[u]:
+           if v in dc.keys(): dc[v]=min(dc[v],dc[u]+1)
+           else: dc[v]=dc[u]+1
+  for u in S:
+       m=m+len(d[u])
+
+  comp.append((len(S),int(m/2),list(S)))
+    
+  #print("Component",comp)
+
+  ST=S
+    
+  #print("ST=",ST)
+
+  #The other components: origin must be outside ST, same treatment
+  i=1
+  while i<len(d):
+   m=0
+   while  list(d.keys())[i] not in ST:
+    a=list(d.keys())[i]
+    S=d[a]
+    S1=set([a])
+    S=S.union(S1)
+    while S > S1:
+      S1=S
+      for u in S:
+         S=S.union(d[u])
+    for u in S:
+       m=m+len(d[u])
+    comp.append((len(S),int(m/2),list(S)))
+    ST=ST.union(S)  
+   i+=1     
+  return comp
+
+
+def comp_edges(comp1):
+ compedges=[]
+ j0=0
+ while j0<len(comp1):
+   l1 = comp1[j0][2]
+   j=0
+   cp=[]
+   while j <len(l1):
+     a=l1[j]
+     j1=0
+     while j1< len(d[l1[j]]):
+   	  f=(a,list(d[l1[j]])[j1])
+   	  cp.append(f)
+   	  j1 +=1
+     j +=1
+   j0 +=1
+   compedges.append(cp)
+ return compedges
+
+
+
+f = open("data/%s_window_reservoir_edges.csv" % (now.strftime("%Y_%m_%d")), "a")
+f.write("Source, Destination, Component_id\n")
+
+def write_edge_reservoir(i, comp_id):
 	now = datetime.datetime.now()
-	f = open("data/%s_window_reservoir_edges.csv" % (now.strftime("%Y_%m_%d_%Hh%M")), "a")
-	f.write("Source, Destination, Timestamp \n")
-	for i in edge_reservoir:
+	f = open("data/%s_window_reservoir_edges.csv" % (now.strftime("%Y_%m_%d")), "a")
+	# f.write("Source, Destination, Component_id\n")
+	for j in i:
 		try:
-			f.write("%s, %s, %s \n" %i)
+			f.write("%s, %s \n" %j)
 		except:
 			pass
 	f.close()
@@ -67,7 +163,7 @@ class StdOutListener(StreamListener):
     This is a basic listener that just prints received tweets to stdout.
 	"""
 	def on_data(self, data):
-		global window_k, L, global_counter, time_counter, interval_counter
+		global window_k, L, global_counter, time_counter, interval_counter, threshold
 		twitter_edges_graph = create_graph(data)
 		if len(twitter_edges_graph) > 0:
 			for edge in twitter_edges_graph:
@@ -86,7 +182,15 @@ class StdOutListener(StreamListener):
 		if time.time()*1000 > start + timeout:
 			print("Nombre de tuple global", L)
 			print("Taille du réservoir", len(window_reservoir_sampling))
-			write_edge_reservoir(window_reservoir_sampling)
+			comp = components(window_reservoir_sampling)
+			comp1=sorted(comp,reverse=True)
+			xx = comp_edges(comp1)
+			comp_id = 0
+			for i in xx:
+				if len(i) >= int(threshold):
+					write_edge_reservoir(i, comp_id)
+					comp_id+=1
+			# print("Edges of each component",xx)
 			sys.exit('Capture terminated')
 		return True
 
