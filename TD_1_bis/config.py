5d7dfede3a658ce4cd87cecc11cfd5a0afaa1bf8
Guillaume VIMONT <vimontguillaume@gmail.com>
2018-11-21 22:14:15 +0100

    Delete config.py

--- TD_1_bis/config.py
+++
@@ -1,7 +0,0 @@
-class TwitterAuth:
-	# Go to http://dev.twitter.com and create an app. 
-	# The consumer key and secret will be generated for you after
-	consumer_key=""
-	consumer_secret=""
-	access_token=""
-	access_token_secret=""
